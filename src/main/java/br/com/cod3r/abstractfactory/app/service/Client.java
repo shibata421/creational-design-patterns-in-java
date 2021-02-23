6d1e9b40cf8a0016b9532da80be6e145a75a6a03
Fernando Shibata <shibata.421@gmail.com>
2021-02-23 10:57:45 -0300

    Implements second abstract factory example

--- src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
+++ src/main/java/br/com/cod3r/abstractfactory/app/service/Client.java
@@ -1,8 +1,22 @@
 package br.com.cod3r.abstractfactory.app.service;
 
+import br.com.cod3r.abstractfactory.app.service.factory.RestAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.factory.ServicesAbstractFactory;
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public class Client {
 
 	public static void main(String[] args) {
-
+//		ServicesAbstractFactory factory = new EJBAbstractFactory();
+		ServicesAbstractFactory factory = new RestAbstractFactory();
+		
+		CarService carService = factory.getCarService();
+		carService.save("Corolla");
+		carService.update("Prius");
+		
+		UserService userService = factory.getUserService();
+		userService.delete(1);
+		userService.save("John");
 	}
 }
--- src/main/java/br/com/cod3r/abstractfactory/app/service/factory/ServicesAbstractFactory.java
+++ src/main/java/br/com/cod3r/abstractfactory/app/service/factory/ServicesAbstractFactory.java
@@ -1,5 +1,10 @@
 package br.com.cod3r.abstractfactory.app.service.factory;
 
+import br.com.cod3r.abstractfactory.app.service.services.CarService;
+import br.com.cod3r.abstractfactory.app.service.services.UserService;
+
 public interface ServicesAbstractFactory {
 
+	CarService getCarService();
+	UserService getUserService();
 }
